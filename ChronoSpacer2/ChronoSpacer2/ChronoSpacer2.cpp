75301269c8347803cef02afae708a02c32bd9038
Takajus <thomas.takagi@edu.devinci.fr>
2020-11-09 18:31:45 +0100

    Projet sans SFML

---
+++ ChronoSpacer2/ChronoSpacer2/ChronoSpacer2.cpp
@@ -0,0 +1,60 @@
+// ChronoSpacer2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+
+
+std::string getAppPath()
+{
+    char cExeFilePath[256];
+    GetModuleFileNameA(NULL, cExeFilePath, 256);
+    std::string exeFilePath = cExeFilePath;
+    int exeNamePos = exeFilePath.find_last_of("\\/");
+    std::string appPath = exeFilePath.substr(0, exeNamePos + 1);
+    return appPath;
+}
+
+//std::string getAssetsPath()
+//{
+//    
+//}
+
+
+int main()
+{
+    std::cout << getAppPath() << "\n";
+    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+    // Initialise everything below
+    sf::CircleShape circle; // Déclaration de "circle" (sans affectation)
+    circle.setRadius(100.0f); // Appel de méthode qui travaille sur "circle"
+    circle.setFillColor(sf::Color::Red);
+    // Game loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Process any input event here
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        // Whatever I want to draw goes here
+        window.draw(circle);
+        window.display();
+    }
+    
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+// Astuces pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
